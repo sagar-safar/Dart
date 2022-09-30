388de7c3fa1a0d6e280475f1c149603b15c53dcb
sagar-safar <sagargupta800016@gmail.com>
2022-09-30 05:12:02 -0700

    string

---
+++ string.dart
@@ -0,0 +1,10 @@
+void main() {
+  String text = "hello";
+  //             01234
+
+  String text2 = "world";
+  print(text.length); // length attribite
+  print(text[0]); // indexing in string
+  print(text.contains('h')); // returns boolean value
+  print(text + text2); //concatenation
+}
