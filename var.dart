619dc885ed67139779c39fb5ada9c5f387895578
sagar-safar <sagargupta800016@gmail.com>
2022-09-30 04:41:30 -0700

    variable

---
+++ var.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "sonu";
+  int age = 23;
+  double weight = 76.5;
+  bool have_gf = false;
+
+  print('my name is ${name}.');
+
+  name = "sagar";
+  print("my new name is ${name}.");
+}
